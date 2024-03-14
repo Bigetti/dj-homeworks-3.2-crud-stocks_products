c860216783a94c6f221d6b93e22cce9e557b2383
bigetti <upgrade81@mail.ru>
2024-03-14 11:28:36 +0300

    New

---
+++ stocks_products/logistic/urls.py
@@ -0,0 +1,12 @@
+from rest_framework.routers import DefaultRouter
+from django.urls import path, include
+from logistic.views import ProductViewSet, StockViewSet
+
+
+router = DefaultRouter()
+router.register('products', ProductViewSet)
+router.register('stocks', StockViewSet)
+# router.register('products', ProductListView)
+
+
+urlpatterns = router.urls
